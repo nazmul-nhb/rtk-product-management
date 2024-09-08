e69f756474957f192882e017c3d7fdd1a597caad
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-09-08 19:27:10 +0600

    created patch route to update single product

--- server/controllers/productControllers.ts
+++ server/controllers/productControllers.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { ProductDetails } from "../types/interfaces";
 import { Product } from "../models/productModel";
 
+// Create Product(s)
 export const createProduct = async (
 	req: Request<{}, {}, ProductDetails | ProductDetails[]>,
 	res: Response
@@ -30,7 +31,7 @@ export const createProduct = async (
 		}
 	} catch (error) {
 		if (error instanceof Error) {
-			console.error("Error Creating Product: ", error.message);
+			console.error("Error Creating Product(s): ", error.message);
 			res.status(400).send({
 				success: false,
 				message: error.message,
@@ -45,6 +46,7 @@ export const createProduct = async (
 	}
 };
 
+// Get all products
 export const getProducts = async (req: Request, res: Response) => {
 	try {
 		const [products, totalProducts] = await Promise.all([
@@ -60,6 +62,45 @@ export const getProducts = async (req: Request, res: Response) => {
 	} catch (error) {
 		if (error instanceof Error) {
 			console.error("Error Fetching Products: ", error.message);
+			res.status(400).send({
+				success: false,
+				message: error.message,
+			});
+		} else {
+			console.error("An Unknown Error Occurred!");
+			res.status(500).send({
+				success: false,
+				message: "Internal Server Error!",
+			});
+		}
+	}
+};
+
+// Update a product by ID
+export const updateProduct = async (
+	req: Request<{ id: string }, {}, ProductDetails>,
+	res: Response
+) => {
+	try {
+		const ID = req.params.id;
+		const product = req.body;
+		const updatedProduct = await Product.findByIdAndUpdate(ID, product, {
+			new: true,
+			runValidators: true,
+		});
+
+		if (updatedProduct) {
+			return res.status(201).send({
+				success: true,
+				updatedProduct,
+				message: `${updatedProduct.title} is Updated Successfully!`,
+			});
+        } else {
+            throw new Error("Cannot Update the Product!")
+        }
+	} catch (error) {
+		if (error instanceof Error) {
+			console.error("Error Updating Product: ", error.message);
 			res.status(400).send({
 				success: false,
 				message: error.message,
--- server/routes/products.ts
+++ server/routes/products.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from "express";
-import { createProduct, getProducts } from "../controllers/productControllers";
+import { createProduct, getProducts, updateProduct } from "../controllers/productControllers";
 
 const router = express.Router();
 
 router.post("/", createProduct);
 router.get("/", getProducts);
+router.patch("/:id", updateProduct);
 
 export default router;
