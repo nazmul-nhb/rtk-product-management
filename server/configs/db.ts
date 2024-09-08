36aca1447e07c4cda6408d86f738957de887c2c9
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-09-08 17:41:33 +0600

    project initialized with server

---
+++ server/configs/db.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const mongoURI = process.env.MONGO_CONNECTION_STRING as string;
+
+export const connectDB = async () => {
+	try {
+		await mongoose.connect(mongoURI);
+		console.log("DB is Connected!");
+	} catch (error) {
+		if (error instanceof Error) {
+			console.error(error.message);
+		} else {
+			console.error("An Unknown Error Occurred!");
+		}
+		console.log("DB is Not Connected!");
+		// process.exit(1);
+	}
+};
---
+++ server/index.ts
@@ -0,0 +1,59 @@
+import express, {Application, Request, Response, NextFunction } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./configs/db";
+import { ErrorObject } from "./types/interfaces";
+
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 4242;
+
+// Use IIFE to start the Server
+(async () => {
+	try {
+		// Connect to DB
+		await connectDB();
+
+		// Middlewares
+		// TODO: Add CORS Options when project is done!
+		app.use(cors());
+		app.use(express.json());
+
+		// Routes
+		app.get("/", async (req: Request, res: Response) => {
+			res.send("Server is Running!");
+		});
+
+		// Error handler for 404
+		app.use((req: Request, res: Response, next: NextFunction) => {
+			const error: ErrorObject = new Error("Requested URL Not Found!");
+			error.status = 404;
+			next(error);
+		});
+
+		// Final error handler
+		app.use(
+			(
+				error: ErrorObject,
+				req: Request,
+				res: Response,
+				next: NextFunction
+			) => {
+				console.error(error);
+				res.status(error.status || 500).send({
+					success: false,
+					message: error.message || "Internal Server Error!",
+				});
+			}
+		);
+
+		// Start the server
+		app.listen(port, () => {
+			console.log("Server is Running on Port: ", port);
+		});
+	} catch (error) {
+		console.error("Failed to Start the Server: ", error);
+		// process.exit(1);
+	}
+})();
