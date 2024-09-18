16d8b384573d1b94f3a90f06bc23f16a0837ef4a
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-09-18 13:55:31 +0600

    show/hide form to add ne product

--- client/src/components/CreateProduct.tsx
+++ client/src/components/CreateProduct.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import toast from "react-hot-toast";
 import { Button, Form, Input } from "antd";
 import { IProductToCreate } from "../types/interfaces";
@@ -6,6 +6,7 @@ import { useCreateProductMutation } from "../features/apiSlice";
 import { TProductField } from "../types/types";
 
 const CreateProduct: React.FC = () => {
+	const [showForm, setShowForm] = useState<boolean>(false);
 	const [createProduct] = useCreateProductMutation();
 	const [form] = Form.useForm();
 
@@ -27,59 +28,74 @@ const CreateProduct: React.FC = () => {
 	};
 
 	return (
-		<section className="w-full px-6 my-6 mx-auto flex flex-col items-center justify-center gap-5">
-			<h3 className="text-xl font-bold text-transparent bg-gradient-to-r from-orange-950 to-teal-900 bg-clip-text">
+		<section
+			className={`w-full px-6 my-6 mx-auto flex flex-col items-center justify-center gap-5 transition-all duration-1000 ease-in-out overflow-hidden origin-top ${
+				showForm ? "max-h-full opacity-100" : "max-h-16 opacity-70"
+			}`}
+		>
+			<h3
+				onClick={() => setShowForm(!showForm)}
+				className="cursor-pointer text-xl font-bold text-transparent bg-gradient-to-r from-orange-950 to-teal-900 bg-clip-text"
+			>
 				Create & Save A New Product
 			</h3>
-			<Form
-				form={form}
-				name="product"
-				style={{ width: "100%", maxWidth: 600 }}
-				onFinish={handleCreateProduct}
-				autoComplete="on"
+			<div
+				className={`w-full flex items-center justify-center transition-all duration-700 ease-in-out origin-center ${
+					showForm
+						? "opacity-100 scale-100 max-h-full"
+						: "opacity-0 scale-0 max-h-0"
+				}`}
 			>
-				<Form.Item<TProductField>
-					name="title"
-					rules={[
-						{
-							required: true,
-							message: "Please input Product Title!",
-						},
-					]}
+				<Form
+					form={form}
+					name="product"
+					style={{ width: "100%", maxWidth: 600 }}
+					onFinish={handleCreateProduct}
+					autoComplete="on"
 				>
-					<Input placeholder="Product Title" />
-				</Form.Item>
+					<Form.Item<TProductField>
+						name="title"
+						rules={[
+							{
+								required: true,
+								message: "Please input Product Title!",
+							},
+						]}
+					>
+						<Input placeholder="Product Title" />
+					</Form.Item>
 
-				<Form.Item<TProductField>
-					name="productImage"
-					rules={[
-						{
-							required: true,
-							message: "Please input Product Image Link!",
-						},
-					]}
-				>
-					<Input placeholder="Product Image" />
-				</Form.Item>
+					<Form.Item<TProductField>
+						name="productImage"
+						rules={[
+							{
+								required: true,
+								message: "Please input Product Image Link!",
+							},
+						]}
+					>
+						<Input placeholder="Product Image" />
+					</Form.Item>
 
-				<Form.Item<TProductField>
-					name="price"
-					rules={[
-						{
-							required: true,
-							message: "Please input Product Price!",
-						},
-					]}
-				>
-					<Input placeholder="Product Price" />
-				</Form.Item>
+					<Form.Item<TProductField>
+						name="price"
+						rules={[
+							{
+								required: true,
+								message: "Please input Product Price!",
+							},
+						]}
+					>
+						<Input placeholder="Product Price" />
+					</Form.Item>
 
-				<Form.Item>
-					<Button type="dashed" htmlType="submit">
-						Save Product
-					</Button>
-				</Form.Item>
-			</Form>
+					<Form.Item>
+						<Button type="dashed" htmlType="submit">
+							Save Product
+						</Button>
+					</Form.Item>
+				</Form>
+			</div>
 		</section>
 	);
 };
